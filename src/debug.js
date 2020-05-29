4b5ddef447fc042deb45bc863114ca062743d04e
rasta361 <n.d.metwli@gmx.de>
2020-05-29 19:57:41 +0200

    changed game loop to be able to set the fps. also moved drawGrid() into debug.js

--- src/debug.js
+++ src/debug.js
@@ -7,3 +7,20 @@ export function drawFPS(context, deltaTime) {
     context.font='10px sans-serif';
     context.fillText('FPS: ' + fps, 10, 30);
 }
+
+export function drawGrid(context, game) {
+    context.fillStyle = '#000';
+    context.lineWidth = 1;
+    // draw vertical lines
+    for(let i=0; i<game.gameWidth/game.gridSize; i++) {
+        context.moveTo(i * game.gridSize, 0);
+        context.lineTo(i * game.gridSize, game.gameHeight)
+    }
+    // draw horizontal lines
+    for(let i=0; i<game.gameHeight/game.gridSize; i++) {
+        context.moveTo(0, i * game.gridSize);
+        context.lineTo(game.gameWidth, i * game.gridSize)
+    }
+    context.closePath();
+    context.stroke();
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Game from './game.js';
-import {drawFPS} from './debug.js';
+import {drawFPS, drawGrid} from './debug.js';
 
 let canvas = document.getElementById('gameScreen');
 let context = canvas.getContext('2d');
@@ -7,21 +7,45 @@ let context = canvas.getContext('2d');
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+let fps, fpsInterval, startTime, now, then, elapsed;
+let lastTime = 0;
+
 let game = new Game(GAME_WIDTH, GAME_HEIGHT);
 game.start();
 
-let lastTime = 0;
+startLoop(10);
+
+function startLoop(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    //console.log(startTime);
+    gameLoop();
+}
 
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
-
-    context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-    game.update(deltaTime);
-    game.draw(context);
-    drawFPS(context, deltaTime);
     
     requestAnimationFrame(gameLoop);
+
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but...
+        // Also, adjust for fpsInterval not being multiple of 16.67
+        then = now - (elapsed % fpsInterval);
+
+        // draw stuff here
+        context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        game.update(deltaTime);
+        game.draw(context);
+        drawFPS(context, deltaTime);
+    }
+    
 }
-requestAnimationFrame(gameLoop);
+//requestAnimationFrame(gameLoop);
