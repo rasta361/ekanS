5c44d76059be0ecba0cdf2d15ec00218f27af856
rasta361 <n.d.metwli@gmx.de>
2020-05-30 22:23:37 +0200

    added tail to snake

--- src/snake.js
+++ src/snake.js
@@ -2,50 +2,43 @@ export default class Snake {
     constructor(game) {
         this.width = game.gridSize;
         this.height = game.gridSize;
+        this.length = 5;
 
-        this.maxSpeed = 10;
+        this.maxSpeed = 5;
         this.speed = {x: 0, y: 0};
 
         this.position = {x: game.gameWidth / 2, y: game.gameHeight / 2};
+        this.tails = [];
 
-        // variables needed to control fps
+        // variables needed to control snake speed
         this.fpsInterval = 1000 / this.maxSpeed;
         this.then = Date.now();
         this.startTime = this.then;
     }
 
     moveLeft(){
-        //this.position.x -= this.speed.x;
-        //this.speed.x -= this.width;
-        if (this.speed.x === this.width)
-            this.speed.x -= this.width * 2;
+        if (this.speed.x === this.width) return;
         else this.speed.x -= this.width;
 
         this.speed.y = 0;
     }
 
     moveRight(){
-        //this.position.x += this.speed.x;
-        if (this.speed.x === -this.width)
-            this.speed.x += this.width * 2;
+        if (this.speed.x === -this.width) return;
         else this.speed.x += this.width;
 
         this.speed.y = 0;
     }
 
     moveUp(){
-        //this.position.y -= this.speed.y;
-        if (this.speed.y === this.width)
-        this.speed.y -= this.height * 2;
+        if (this.speed.y === this.width) return;
         else this.speed.y -= this.height;
 
         this.speed.x = 0;
     }
 
     moveDown(){
-        //this.position.y += this.speed.y;
-        if (this.speed.y === -this.width)
-            this.speed.y += this.height * 2;
+        if (this.speed.y === -this.width) return;
         else this.speed.y += this.height;
 
         this.speed.x = 0;
@@ -58,6 +51,10 @@ export default class Snake {
     draw(context) {
         context.fillStyle='#459f30';
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        for(let i=0; i < this.tails.length; i++){
+            context.fillRect(this.tails[i][0], this.tails[i][1], this.width, this.height);
+        }
     }
 
     update(deltaTime) {
@@ -65,12 +62,32 @@ export default class Snake {
         this.now = Date.now();
         this.elapsed = this.now - this.then;
 
-        // if enough time has elapsed, draw the next frame
+        console.clear();
+        console.log('position x: ' + this.position.x + ' position y: ' + this.position.y);
+        console.log('tails: ' + this.tails.length);
+
+        // if enough time has elapsed, update the next frame
         if (this.elapsed > this.fpsInterval) {
+            // add values of position to tails array
+            if (this.tails.length < this.length) this.tails.push(Object.values(this.position));
+
+            // each tail follows the one before
+            for(let i = this.tails.length; i > 1; i--) {
+                this.tails[i-1][0] = this.tails[i-2][0];
+                this.tails[i-1][1] = this.tails[i-2][1];
+            }
+
+            // first tail follows the head
+            if (this.tails != 0) {
+                this.tails[0][0] = this.position.x;
+                this.tails[0][1] = this.position.y;
+            }
+
             // Get ready for next frame by setting then=now, but...
             // Also, adjust for fpsInterval not being multiple of 16.67
             this.then = this.now - (this.elapsed % this.fpsInterval);
             
+            // update head position
             this.position.x += this.speed.x;
             this.position.y += this.speed.y;
         }
